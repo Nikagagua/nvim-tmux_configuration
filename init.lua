4e18faef73a21a40ee9e3f665f0abcddf3072c12
Nika Gagua <Nikagagua@live.com>
2024-07-21 22:54:51 +0400

    Configured init and added terminal

--- init.lua
+++ init.lua
@@ -1,28 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Error handling
-local status, lazy = pcall(require, "lazy")
-if not status then
-	print("Error loading lazy.nvim: " .. lazy)
-	return
-end
-
-status, err = pcall(lazy.setup, "plugins")
-if not status then
-	print("Error setting up plugins: " .. err)
-	return
-end
-
--- Load other configurations
 require("vim-options")
+require("lazy").setup("plugins")
+
